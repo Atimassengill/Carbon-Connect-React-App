7727389886e57c20a1e020c6ef638e978ad08533
Atimassengill <massengill.ati@gmail.com>
2021-03-09 21:57:33 -0600

    second commit

---
+++ src/Com/pages/Calculator.js
@@ -0,0 +1,18 @@
+import '../../App.css';
+import React from 'react';
+
+
+
+
+export default function Calculator() {
+    
+    return (
+        <div className="Clac">
+            
+            <h1>The Carbon Calculator</h1>
+            <>
+            <iframe width='100%' height="1500" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" src="https://calculator.carbonfootprint.com/calculator.aspx"></iframe>
+            </>
+        </div>
+    )
+}
